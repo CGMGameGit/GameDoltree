26836523fbe7d20e77fe20ec6cd9d4be6223c746
Natakorn <chuck.nk.kn@gmail.com>
2016-02-16 14:24:37 +0700

    New folder Scenes folder Scripts , prefab

---
+++ GameDoltree/Assets/Scripts/spawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn : MonoBehaviour {
+
+	public GameObject BoxPrefab;
+	public GameObject[] aaa;
+	GameObject xxx;
+
+	private int positionobject;
+
+	void Start()
+	{
+		InvokeRepeating ("spawnBox", 1f, 3f);
+	}
+
+	void Update ()
+	{
+
+	}
+
+	void spawnBox ()
+	{
+		positionobject = (int)Random.Range (0, 4);
+
+		xxx = Instantiate(BoxPrefab,aaa[positionobject].transform.position , Quaternion.identity) as GameObject;
+
+	}
+}
